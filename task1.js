4c95e0c450acc58ff8b49624ca7f1cd680fb77c6
Abylay Kuvatov <abukuvatov@icloud.com>
2023-03-31 17:53:35 +0600

    Добавил скрипт для задания 1

---
+++ task1.js
@@ -0,0 +1,9 @@
+const list = ['молоко', 'пиво', 'пиво', 'молоко', 'молоко'];
+for (let i = 0; i < list.length; i++) {
+    if (list[i] === 'молоко') {
+        console.log('хорошо');
+    }
+    else {
+        console.log('плохо');
+    }
+}
